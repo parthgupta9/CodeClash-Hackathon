594d628101690afcefd1b706b2bc411d706e22de
parthgupta9 <parth615gupta@gmail.com>
2024-04-19 20:34:08 +0530

    third commit

--- homepage/src/components/Landing.js
+++ homepage/src/components/Landing.js
@@ -1,11 +1,36 @@
 import React from 'react'
-import landing from '../assets/Frame 18 (1).png'
-import '../components/navbar.css'
+import anding from '../assets/Frame 18 (1).png'
+import imgg from '../assets/down.png'
 
 const Landing = () => {
   return (
-    <div className='landing'><img src={landing} style={{width:"100%",
-height:"870px",padding:"-1px",margin:"0px"}}/></div>
+    <div><div id="carouselExampleIndicators" className="carousel slide">
+    <div className="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img src={anding} className="d-block w-100" alt="..."/>
+      </div>
+      <div className="carousel-item">
+        <img src={anding} className="d-block w-100" alt="..."/>
+      </div>
+      <div className="carousel-item">
+        <img src={anding} className="d-block w-100" alt="..."/>
+      </div>
+    </div>
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+    {/* <img src={imgg} style={{marginTop:"-80px"}}></img> */}
+  </div></div>
   )
 }
 
