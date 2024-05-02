d6505c564a24b26f4cf73866411a45fd74925863
navneetnavs <navneetkumar2004@icloud.com>
2024-05-03 01:20:05 +0530

    commit

--- homepage/src/hotels/api/travelAdvisorAPI.js
+++ homepage/src/hotels/api/travelAdvisorAPI.js
@@ -25,3 +25,23 @@ try {
 } catch (error) {
 	console.error(error);
 }
+
+
+
+// weather api content
+
+const options = {
+  method: 'GET',
+  url: 'https://open-weather-map27.p.rapidapi.com/weather',
+  headers: {
+    'X-RapidAPI-Key': 'a1c4765807msh4bbbdbaf58eb6c8p1ab2adjsne47c8f4b67c5',
+    'X-RapidAPI-Host': 'open-weather-map27.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
